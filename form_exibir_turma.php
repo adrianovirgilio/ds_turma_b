56b3b5affed9960215c02988deb4ae837897647f
adrianovirgilio <49097002+adrianovirgilio@users.noreply.github.com>
2019-04-23 20:16:29 -0300

    Update form_exibir_turma.php

--- form_exibir_turma.php
+++ form_exibir_turma.php
@@ -1,4 +1,13 @@
-﻿<!doctype html>
+<!--
+ # Etec Tenente Aviador Gustavo Klug
+ # Curso: Informática Para Internet
+ # 3º Módulo -Turma B
+ # Disciplina: Desenvolvimento de Software II
+ # Competências: Analisar a orientação a objetos e sua aplicação em sistemas web; Integrar sistemas web com sistemas gerenciadores de banco de dados.
+ # Prof. Adriano Virgílio
+ # Data: 23/04/2019
+-->
+<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
